baa4ff066a82dda0a78790ecb9fb390d29b57526
mathisct <mathisct@mail.uc.edu>
2022-12-04 16:18:08 -0500

    datacontext

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using ContempProgrammingFinal;
-
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddDbContext<DbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerDocument();
